253719a3f142cdcdb2531d0a0272cd52ebd25a18
Justin Elliott <justindelliott@gmail.com>
2020-05-24 16:38:30 -0400

    Commented out the run of the container since this script should only build it.

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ DOCKER_RUN_IMAGE=nagios
 docker build -t "${DOCKER_RUN_IMAGE}" .
 
 docker images
-docker run -d --rm --name "${DOCKER_RUN_IMAGE}" -p 8080:80 -t "${DOCKER_RUN_IMAGE}"
+# docker run -d --rm --name "${DOCKER_RUN_IMAGE}" -p 8080:80 -t "${DOCKER_RUN_IMAGE}"
 
